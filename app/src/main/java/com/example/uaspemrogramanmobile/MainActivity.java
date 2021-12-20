d494f635783dd341759578c91ad49859e96d5cbe
ihsanfj55 <ihsanfj55@gmail.com>
2021-12-20 12:14:12 +0800

    Volley-APP UAS

---
+++ app/src/main/java/com/example/uaspemrogramanmobile/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.uaspemrogramanmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private final String JSON_URL = "https://run.mocky.io/v3/29f01f34-9413-4c28-acb8-745b168f4e84" ;
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Anime> lstAnime ;
+    private RecyclerView recyclerView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstAnime = new ArrayList<>() ;
+        recyclerView = findViewById(R.id.recyclerviewid);
+        jsonrequest();
+    }
+
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Anime anime = new Anime() ;
+                        anime.setName(jsonObject.getString("name"));
+                        anime.setDescription(jsonObject.getString("description"));
+                        anime.setRating(jsonObject.getString("Rating"));
+                        anime.setCategorie(jsonObject.getString("categorie"));
+                        anime.setNb_episode(jsonObject.getInt("episode"));
+                        anime.setStudio(jsonObject.getString("studio"));
+                        anime.setImage_url(jsonObject.getString("img"));
+                        lstAnime.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+                setuprecyclerview(lstAnime);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request) ;
+
+
+    }
+
+    private void setuprecyclerview(List<Anime> lstAnime) {
+
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(this, lstAnime) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myadapter);
+
+    }
+}
